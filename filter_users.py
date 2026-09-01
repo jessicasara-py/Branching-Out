fa855a690f22a16cace0b6d7c2b9b7ca8c49a8fa
jessicasara-py <j.saratoff@gmail.com>
2026-09-01 18:48:44 +0200

    added function filter_users_by_email to filter_users.py

--- filter_users.py
+++ filter_users.py
@@ -26,3 +26,12 @@ def filter_users_by_age(age):
 
   for user in filtered_users:
       print(user)
+
+def filter_users_by_email(email):
+  with open("users.json", "r") as file:
+      users = json.load(file)
+
+  filtered_users = [user for user in users if user.get("email", "").lower() == email.lower()]
+
+  for user in filtered_users:
+      print(user)
